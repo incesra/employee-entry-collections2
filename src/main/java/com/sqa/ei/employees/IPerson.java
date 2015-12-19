d13aa301a39a872fa58af15dee396c0a06ad6f09
Esra Ince <ince.esras@gmail.com>
2015-12-19 15:56:30 -0800

    Creating IPerson Interface

---
+++ src/main/java/com/sqa/ei/employees/IPerson.java
@@ -0,0 +1,33 @@
+/**
+ *   File Name: IPerson.java<br>
+ *
+ *   Ince, Esra<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Dec 19, 2015
+ *
+ */
+
+package com.sqa.ei.employees;
+
+/**
+ * IPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Ince, Esra
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public interface IPerson {
+
+	final static int MAX_AGE = 110;
+
+	public String getName();
+
+	public boolean move();
+
+}
